502aa718e4656ae132869ce6097269e7ce576bad
Adrien Lindner <lindner.54@osu.edu>
2019-09-23 22:29:04 -0400

    Inline svg in td scales vertically

--- src/app/components/table-with-svg/table-with-svg.component.ts
+++ src/app/components/table-with-svg/table-with-svg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-table-with-svg',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-with-svg.component.scss']
 })
 export class TableWithSvgComponent implements OnInit {
+  faCoffee = faCoffee;
 
   constructor() { }
 
